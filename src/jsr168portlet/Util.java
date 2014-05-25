90c78320a54e79c2884c0a9176a153832bb27071
serverkabure <serverkabure@live.jp>
2014-05-26 06:29:22 +0900

    openshift-ok

--- src/jsr168portlet/Util.java
+++ src/jsr168portlet/Util.java
@@ -4,15 +4,14 @@ import java.io.File;
 
 public class Util {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		File folder = new File(
 				"C:/eclipse-jee-juno-SR2-win32/workspace/Jsr168Portlet/WebContent/WEB-INF/lib");
 		File[] files = folder.listFiles();
 		for (File file : files) {
-			if (file.getName().endsWith("-sources.jar")) {
+			String filename = file.getName();
+			if (filename.endsWith("-sources.jar")
+					|| filename.endsWith("-javadoc.jar")) {
 				file.delete();
 			}
 		}
