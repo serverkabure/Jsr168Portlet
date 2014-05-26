c1127abca728a16ba3ae8bca09a4344f99115b11
serverkabure <serverkabure@live.jp>
2014-05-27 02:08:21 +0900

    GitHub

--- src/jsr168portlet/Util.java
+++ src/jsr168portlet/Util.java
@@ -1,12 +1,23 @@
 package jsr168portlet;
 
 import java.io.File;
+import java.util.Map;
 
 public class Util {
-
 	public static void main(String[] args) {
-		File folder = new File(
-				"C:/eclipse-jee-juno-SR2-win32/workspace/Jsr168Portlet/WebContent/WEB-INF/lib");
+		// System.out.println(new File(".").getAbsolutePath());
+		main0(null);
+	}
+
+	public static void main1(String[] args) {
+		Map<String, String> map = System.getenv();
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			System.out.printf("%s, %s%n", entry.getKey(), entry.getValue());
+		}
+	}
+
+	public static void main0(String[] args) {
+		File folder = new File("./WebContent/WEB-INF/lib");
 		File[] files = folder.listFiles();
 		for (File file : files) {
 			String filename = file.getName();
