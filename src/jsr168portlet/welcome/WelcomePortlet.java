4cb0984ef3c42fb4c4755ee407c177cac3693dd4
serverkabure <serverkabure@live.jp>
2014-06-05 05:39:58 +0900

    preferense-ok

--- src/jsr168portlet/welcome/WelcomePortlet.java
+++ src/jsr168portlet/welcome/WelcomePortlet.java
@@ -1,6 +1,7 @@
 package jsr168portlet.welcome;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 import javax.portlet.*;
 
@@ -15,11 +16,34 @@ public class WelcomePortlet extends GenericPortlet {
 		helpPage = config.getInitParameter("helpPage");
 	}
 
+	public void processAction(ActionRequest request, ActionResponse response)
+			throws PortletException {
+		String html = request.getParameter("html");
+		try {
+			PortletPreferences pref = request.getPreferences();
+			pref.setValue("html", html);
+			pref.store();
+		} catch (Exception e) {
+			throw new PortletException(e.getMessage());
+		}
+		// return the user back to the view mode and normal state
+		// response.setPortletMode(PortletMode.VIEW);
+		// response.setWindowState(WindowState.NORMAL);
+	}
+
 	@Override
 	protected void doView(RenderRequest request, RenderResponse response)
 			throws PortletException, IOException {
+		PortletPreferences pref = request.getPreferences();
+		String html = pref.getValue("html", "いらっしゃい");
+		//
+		// response.setContentType(request.getResponseContentType());
+		// PrintWriter writer = response.getWriter();
+		// writer.write("<form><p> " + html +
+		// "</p><p>日本語テスト。①Ⅰⅰ、～㎜㈱。</p></form>");
 		PortletRequestDispatcher dispatcher = getPortletContext()
 				.getRequestDispatcher(viewPage);
+		request.setAttribute("html", html);
 		dispatcher.include(request, response);
 	}
 
